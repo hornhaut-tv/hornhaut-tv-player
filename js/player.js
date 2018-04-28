63dfa688428f7d849fd95141f8f335c0a26f2ff2
Neobald Hornrich <hornrich@hornhaut.tv>
2018-04-28 21:58:23 +0200

    Fix javascript repeat mechanism

--- js/player.js
+++ js/player.js
@@ -1,38 +1,40 @@
 $(function() {
-	var $winHeight = $(window).height();
-	var $winWidth = $(window).width();
+    var $winHeight = $(window).height();
+    var $winWidth = $(window).width();
     $('#container').height($winHeight).width($winWidth);
     $('#container > iframe').height($winHeight).width($winWidth);
 
-    setInterval(execSwitch(), 15000);
+    switchChannel();
 });
 
-function execSwitch() {
-	var channels = [
-		// HORNHAUT Streamers
-		'neobaldhornrich',
-		'banksider',
-		'ndee171',
-		'kokain_',
+function switchChannel() {
+    var channels = [
+        // HORNHAUT Streamers
+        'neobaldhornrich',
+        'banksider',
+        'ndee171',
+        'kokain_',
 
-		// Non-HORNHAUT Streamers
-		'bangpowwww',
-		'caarla',
-		'4thespada90',
-		'dieleonieten',
-		'grubsiud'
-	];
+        // Non-HORNHAUT Streamers
+        'seyyn',
+        'bangpowwww',
+        'caarla',
+        '4thespada90',
+        'dieleonieten',
+        'grubsiud'
+    ];
 
-	var channel = channels[Math.floor(Math.random()*channels.length)];
-	console.log(channel);
-	switchChannel(channel);
+    var channel = channels[Math.floor(Math.random()*channels.length)];
+    console.log(channel);
+    outputChannel(channel);
+    setTimeout(switchChannel, 30000);
 }
 
-function switchChannel(channel) {
-	var urlBase = 'https://player.twitch.tv/?volume=0&!muted&channel=';
-	var url = urlBase + channel;
-	var $channelName = $('#channel-name').text(channel);
-	// $channelName.
+function outputChannel(channel) {
+    var urlBase = 'https://player.twitch.tv/?volume=0&muted&channel=';
+    var url = urlBase + channel;
+    var $channelName = $('#channel-name').text(channel);
+    // $channelName.
     var $iframe = $('#container > iframe');
     if($iframe.length) {
         $iframe.attr('src', url);   
